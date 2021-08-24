2c57f397f1583799f416e9b3589ff7431291e446
Stefanos Toufexis <stefetoufe@gmail.com>
2021-08-24 14:06:19 +0300

    Chore: Fix imports

--- src/channel.ts
+++ src/channel.ts
@@ -1,12 +1,12 @@
 import { Channel, Connection, connect } from 'amqplib';
 
-import { from, Observable } from "rxjs";
-import { flatMap, tap } from "rxjs/internal/operators";
+import { flatMap, from, Observable, tap } from 'rxjs';
 
 export const getChannel = (
   connectionUri: string,
   queueName: string
-): Observable<Channel> => from(connect(connectionUri))
-    .pipe(
-      flatMap((connection: Connection) => from(connection.createChannel())),
-      tap((channel: Channel) => channel.assertQueue(queueName)));
+): Observable<Channel> =>
+  from(connect(connectionUri)).pipe(
+    flatMap((connection: Connection) => from(connection.createChannel())),
+    tap((channel: Channel) => channel.assertQueue(queueName))
+  );
--- src/index.ts
+++ src/index.ts
@@ -2,23 +2,22 @@ import { getChannel } from './channel';
 import { getPublisher } from './publisher';
 import { getEnv } from './utils/get-env';
 import dotenv from 'dotenv';
-import { schemas } from './schemas';
+import { getSchema } from './schemas';
 import { validate } from './validation/configuration-validation';
 import { parseBoolean } from './utils/helpers';
 import { Configuration, ParsedSchema } from './types';
 import { Channel } from 'amqplib';
 
-import { from } from "rxjs";
-import { flatMap, tap } from "rxjs/internal/operators";
+import { flatMap, from, tap } from 'rxjs';
 
 const main = async () => {
   dotenv.config();
 
   const config: Configuration = {
     connectionUri: getEnv('RABBIT_URI'),
-    queueName: getEnv('QUEUE'),
-    schema: schemas[getEnv('SCHEMA')],
-    intervalMillis: parseInt(getEnv('INTERVAL')),
+    queueName: getEnv('RABBIT_QUEUE'),
+    schema: getSchema(getEnv('SCHEMA')),
+    intervalMillis: parseInt(getEnv('RABBIT_INTERVAL')),
     isEnvironmentInstance: parseBoolean(getEnv('IS_INSTANCE', true)),
     host: getEnv('HOST', true),
   };
@@ -27,16 +26,23 @@ const main = async () => {
 
   const { connectionUri, queueName, schema, intervalMillis } = config;
 
-  from(getChannel({ connectionUri, queueName, }))
+  from(getChannel(connectionUri, queueName))
     .pipe(
-      flatMap((channel: Channel) => from(getPublisher({
-        channel,
-        queueName,
-        schema,
-        intervalMillis,
-      }))),
-      tap((parsedSchema: ParsedSchema) => console.log(parsedSchema)))
-    .subscribe();
+      flatMap((channel: Channel) =>
+        from(
+          getPublisher({
+            channel,
+            queueName,
+            schema,
+            intervalMillis,
+          })
+        )
+      )
+    )
+    .subscribe({
+      next: console.log,
+      error: err => console.log(`Error_${err}`),
+    });
 };
 
 main();
